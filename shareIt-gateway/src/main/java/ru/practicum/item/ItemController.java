fc9a2fd6a9376564ff0664b8a1f186adc948da4f
Kenga <sng.inc@yandex.ru>
2024-05-09 16:20:58 +0500

    feat: настроил взаимодействие модулей через ItemClient, все тесты User и Item в Postman выполняются.

--- shareIt-gateway/src/main/java/ru/practicum/item/ItemController.java
+++ shareIt-gateway/src/main/java/ru/practicum/item/ItemController.java
@@ -21,47 +21,48 @@ import static ru.practicum.constans.Constants.REQUEST_HEADER_NAME;
 @AllArgsConstructor
 @RequestMapping("/items")
 public class ItemController {
+    private ItemClient itemClient;
 
 
-//    @PostMapping
-//    public ItemDto createItem(@RequestHeader(REQUEST_HEADER_NAME) long userId,
-//                              @Validated(Marker.OnCreate.class) @RequestBody ItemDto item) {
-//        log.info("Выполняется запрос создания вещи");
-//        return itemService.createItem(userId, item);
-//    }
-//
-//    @PatchMapping("/{id}")
-//    public ItemDto updateItem(@RequestHeader(REQUEST_HEADER_NAME) long userId,
-//                              @PathVariable long id,
-//                              @RequestBody ItemDto item) {
-//        log.info("Выполняется запрос обновления вещи под ID: {} пользователя под ID: {}", id, userId);
-//        return itemService.updateItem(userId, id, item);
-//    }
-//
-//    @GetMapping("/{itemId}")
-//    public ItemDtoWithBookingAndComments getItemById(@RequestHeader(REQUEST_HEADER_NAME) long userId,
-//                                                     @PathVariable long itemId) {
-//        log.info("Выполняется запрос получения информации вещи по ID: {}, от пользователя: {}", itemId, userId);
-//        return itemService.getItemById(userId, itemId);
-//    }
-//
-//    @GetMapping
-//    public List<ItemDtoWithBookingAndComments> getAllItemByOwner(@RequestHeader(REQUEST_HEADER_NAME) long userId) {
-//        log.info("Выполняется запрос получения всех вещей пользователя под ID: {}", userId);
-//        return itemService.getAllItemByOwner(userId);
-//    }
-//
-//    @GetMapping("/search")
-//    public List<ItemDto> searchByNameAndDescription(@RequestParam String text) {
-//        log.info("Выполняется запрос поиска по имени и описанию. Текст запроса: {}", text);
-//        return itemService.search(text);
-//    }
-//
-//    @PostMapping("/{itemId}/comment")
-//    public CommentDto createComment(@RequestHeader(REQUEST_HEADER_NAME) long userId,
-//                                    @PathVariable long itemId,
-//                                    @Validated(Marker.OnCreate.class) @RequestBody CommentDto commentDto) {
-//        log.info("Выполняется запрос создания отзыва для вещи под ID: {}, от пользователя: {}", itemId, userId);
-//        return itemService.createComment(userId, itemId, commentDto);
-//    }
+    @PostMapping
+    public ItemDto createItem(@RequestHeader(REQUEST_HEADER_NAME) long userId,
+                              @Validated(Marker.OnCreate.class) @RequestBody ItemDto item) {
+        log.info("Выполняется запрос создания вещи");
+        return itemClient.create(userId, item);
+    }
+
+    @PatchMapping("/{id}")
+    public ItemDto updateItem(@RequestHeader(REQUEST_HEADER_NAME) long userId,
+                              @PathVariable long id,
+                              @RequestBody ItemDto item) {
+        log.info("Выполняется запрос обновления вещи под ID: {} пользователя под ID: {}", id, userId);
+        return itemClient.updateItem(userId, id, item);
+    }
+
+    @GetMapping("/{itemId}")
+    public ItemDtoWithBookingAndComments getItemById(@RequestHeader(REQUEST_HEADER_NAME) long userId,
+                                                     @PathVariable long itemId) {
+        log.info("Выполняется запрос получения информации вещи по ID: {}, от пользователя: {}", itemId, userId);
+        return itemClient.getItemById(userId, itemId);
+    }
+
+    @GetMapping
+    public List<ItemDtoWithBookingAndComments> getAllItemByOwner(@RequestHeader(REQUEST_HEADER_NAME) long userId) {
+        log.info("Выполняется запрос получения всех вещей пользователя под ID: {}", userId);
+        return itemClient.getAllItemByOwner(userId);
+    }
+
+    @GetMapping("/search")
+    public List<ItemDto> searchByNameAndDescription(@RequestParam String text) {
+        log.info("Выполняется запрос поиска по имени и описанию. Текст запроса: {}", text);
+        return itemClient.search(text);
+    }
+
+    @PostMapping("/{itemId}/comment")
+    public CommentDto createComment(@RequestHeader(REQUEST_HEADER_NAME) long userId,
+                                    @PathVariable long itemId,
+                                    @Validated(Marker.OnCreate.class) @RequestBody CommentDto commentDto) {
+        log.info("Выполняется запрос создания отзыва для вещи под ID: {}, от пользователя: {}", itemId, userId);
+        return itemClient.createComment(userId, itemId, commentDto);
+    }
 }
